f1c875ba5bc9dba1ec9b842d28ffbc854d9a4c9e
hamuf <hamutalf@gmail.com>
2019-03-06 22:46:35 +0200

    dietary restrictions select list + selected restrictions

--- app/shared/nav/navCtrl.js
+++ app/shared/nav/navCtrl.js
@@ -1,6 +1,6 @@
 app.controller("navCtrl", function ($scope, $location) {
     // $scope.isLoggedIn = true;
-    $scope.connected = true;
+    $scope.connected = false;
     $scope.login = function(disconnect) {
         // alert(typeof disconnect);
         $scope.connected = disconnect;
