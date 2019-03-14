dd0e4742ba1890e7e7eff32e9470e0276633b8ef
hamuf <hamutalf@gmail.com>
2019-03-14 15:47:25 +0200

    Upload image preview: Improve page display

--- app/recipes/recipesCtrl.js
+++ app/recipes/recipesCtrl.js
@@ -6,25 +6,25 @@ app.controller("recipesCtrl", function ($scope, $location,recipesSrv,userSrv) {
 
     $scope.recipes = [];
 
-    if (userSrv.getActiveUser()) {
-      $scope.recipeOwnerId = userSrv.getActiveUser().id;
-      console.log($scope.recipeOwnerId);
+    // if (userSrv.getActiveUser()) {
+    //   $scope.recipeOwnerId = userSrv.getActiveUser().id;
+    //   console.log($scope.recipeOwnerId);
 
-      var newRecipe = {
-        name: "עוגת דבש תמרים טבעונית",
-        source: " לומדים לבשל טבעוני | סיו-פוד",
-        sourceUrl: "http://www.sivfood.com/2012/09/datehoneyvegancake.html",
-        description: "המתכון המלא בקישור למעלה, כאן מתכון לחצי כמות, שמתאים לתבנית של 12 מאפינס בינוניים, או תבנית אינגליש קייק         עשיתי שינויים קלים. יש הרבה רעיונות ועצות בהערות של המתכון המקורי",
-        isPublic: "true",
-        owner: userSrv.getActiveUser().id
-      }
+    //   var newRecipe = {
+    //     name: "עוגת דבש תמרים טבעונית",
+    //     source: " לומדים לבשל טבעוני | סיו-פוד",
+    //     sourceUrl: "http://www.sivfood.com/2012/09/datehoneyvegancake.html",
+    //     description: "המתכון המלא בקישור למעלה, כאן מתכון לחצי כמות, שמתאים לתבנית של 12 מאפינס בינוניים, או תבנית אינגליש קייק         עשיתי שינויים קלים. יש הרבה רעיונות ועצות בהערות של המתכון המקורי",
+    //     isPublic: "true",
+    //     owner: userSrv.getActiveUser().id
+    //   }
 
       // console.log(newRecipe);      
       // recipesSrv.createRecipe(newRecipe).then(function() {
       // }, function (err) {
       //   console.log(err);
       // })       
-    }
+    // }
 
     
     // fetch existing pre defined recipes from the model
