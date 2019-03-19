d9bc785f58cd06c167ca2d63bd13bcfaca7efefb
hamuf <hamutalf@gmail.com>
2019-03-19 14:26:58 +0200

    add edit button for users recipes in all the displays

--- app/recipes/recipesCtrl.js
+++ app/recipes/recipesCtrl.js
@@ -3,15 +3,19 @@ app.controller("recipesCtrl", function ($scope, $location, recipesSrv, userSrv) 
 
   // recipes list
   $scope.recipes = [];
+  $scope.ownerId = "";
 
   $scope.isUserRecipePage = ($location.url().indexOf("my-recipes") > 0 && $scope.activeUser !== null);
   // navigate to public recipes page, if no active user
   if (!userSrv.getActiveUser()) {
     $location.path("/");
-
+  } else {
+    $scope.ownerId = userSrv.getActiveUser().id;
+    console.log($scope.ownerId);
   }
 
   $scope.byPropName = "views";
+
 
   // fetch existing pre defined recipes from the model
   recipesSrv.getRecipes($scope.isUserRecipePage).then(function (recipes) {
